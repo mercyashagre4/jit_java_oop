6fbc61b49c0d4628e846ed6478d9e886d4d032d8
mercyashagre4 <118175416+mercyashagre4@users.noreply.github.com>
2023-01-24 15:25:30 -0800

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author WIN-10
+ */
+public class LoopW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i=0;
+        while(i<10){
+            if(i%2==0)
+                System.out.println(i);
+            i++;
+        }
+        // TODO code application logic here
+    }
+    
+}
