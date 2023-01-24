bc3ee21aa25c74e3e4272b37e917da3a51ca76ff
mercyashagre4 <118175416+mercyashagre4@users.noreply.github.com>
2023-01-24 15:37:58 -0800

    Add files via upload

---
+++ doWhile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author WIN-10
+ */
+public class LoopDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int i=0;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }while(i>5);
+        System.out.println("false"+i);
+        // TODO code application logic here
+    }
+}
